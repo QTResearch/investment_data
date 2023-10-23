23770870bfbf6674d75c80f2c4534ced57ecca24
Andy Li <andy.lee1007@hotmail.com>
2023-10-23 17:09:59 +0000

    update qlib bin dir

--- dump_qlib_bin.sh
+++ dump_qlib_bin.sh
@@ -2,6 +2,7 @@ set -e
 set -x
 WORKING_DIR=${1} 
 QLIB_REPO=${2:-https://github.com/microsoft/qlib.git} 
+QLIB_BIN=.qlib/qlib_data/cn_data #qlib_bin
 
 if ! command -v dolt &> /dev/null
 then
@@ -26,13 +27,13 @@ python3 ./qlib/dump_all_to_qlib_source.py
 
 export PYTHONPATH=$PYTHONPATH:$WORKING_DIR/qlib/scripts
 python3 ./qlib/normalize.py normalize_data --source_dir ./qlib/qlib_source/ --normalize_dir ./qlib/qlib_normalize --max_workers=16 --date_field_name="tradedate" 
-python3 $WORKING_DIR/qlib/scripts/dump_bin.py dump_all --csv_path ./qlib/qlib_normalize/ --qlib_dir $WORKING_DIR/qlib_bin --date_field_name=tradedate --exclude_fields=tradedate,symbol
+python3 $WORKING_DIR/qlib/scripts/dump_bin.py dump_all --csv_path ./qlib/qlib_normalize/ --qlib_dir $WORKING_DIR/$QLIB_BIN --date_field_name=tradedate --exclude_fields=tradedate,symbol
 
 mkdir -p ./qlib/qlib_index/
 python3 ./qlib/dump_index_weight.py 
-python3 ./tushare/dump_day_calendar.py $WORKING_DIR/qlib_bin/
+python3 ./tushare/dump_day_calendar.py $WORKING_DIR/$QLIB_BIN/
 killall dolt
 
-cp qlib/qlib_index/csi* $WORKING_DIR/qlib_bin/instruments/
+cp qlib/qlib_index/csi* $WORKING_DIR/$QLIB_BIN/instruments/
 
-tar -czvf ./qlib_bin.tar.gz $WORKING_DIR/qlib_bin/
+tar -czvf ./qlib_bin.tar.gz $WORKING_DIR/$QLIB_BIN/
